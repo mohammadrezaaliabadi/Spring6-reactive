be8f6c348540b179e3aa12370c6481747d338245
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-19 15:17:25 +0330

    Add create and update data close #5

--- src/test/java/com/example/spring6reactive/repositories/BeerRepositoryTest.java
+++ src/test/java/com/example/spring6reactive/repositories/BeerRepositoryTest.java
@@ -1,15 +1,18 @@
 package com.example.spring6reactive.repositories;
 
+import com.example.spring6reactive.config.DatabaseConfig;
 import com.example.spring6reactive.domain.Beer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataR2dbcTest
+@Import(DatabaseConfig.class)
 class BeerRepositoryTest {
     @Autowired
     BeerRepository beerRepository;
