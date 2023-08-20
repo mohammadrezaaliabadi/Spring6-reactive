abee6c3036693d2957838d0de19a0f32b0d9630e
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-20 23:43:48 +0330

    Create new entity close #11

--- src/main/java/com/example/spring6reactive/controllers/BeerController.java
+++ src/main/java/com/example/spring6reactive/controllers/BeerController.java
@@ -3,9 +3,9 @@ package com.example.spring6reactive.controllers;
 import com.example.spring6reactive.model.BeerDTO;
 import com.example.spring6reactive.services.BeerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -15,14 +15,24 @@ public class BeerController {
     public static final String BEER_PATH = "/api/v2/beer";
     public static final String BEER_PATH_ID = BEER_PATH + "/{beerId}";
     private final BeerService beerService;
+
+    @PostMapping(BEER_PATH)
+    Mono<ResponseEntity<Void>> createNewBeer(@RequestBody BeerDTO beerDTO) {
+        return beerService.saveNewBeer(beerDTO)
+                .map(savedDto ->
+                        ResponseEntity.created(UriComponentsBuilder
+                                .fromHttpUrl("http://localhost:8080/" + BEER_PATH
+                                        + "/" + savedDto.getId()).build().toUri()).build());
+    }
+
     @GetMapping(BEER_PATH_ID)
-    Mono<BeerDTO> getBeerById(@PathVariable("beerId") Integer beerId){
+    Mono<BeerDTO> getBeerById(@PathVariable("beerId") Integer beerId) {
         return beerService.getBeerById(beerId);
     }
 
 
     @GetMapping(BEER_PATH)
-    Flux<BeerDTO> listBeers(){
+    Flux<BeerDTO> listBeers() {
         return beerService.listBeers();
     }
 }
--- src/main/java/com/example/spring6reactive/services/BeerService.java
+++ src/main/java/com/example/spring6reactive/services/BeerService.java
@@ -7,4 +7,5 @@ import reactor.core.publisher.Mono;
 public interface BeerService {
     Flux<BeerDTO> listBeers();
     Mono<BeerDTO> getBeerById(Integer beerId);
+    Mono<BeerDTO> saveNewBeer(BeerDTO beerDTO);
 }
