a98b6fb3b673c9ef8ffbc236ba92d63ff4fb2c77
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-20 23:39:13 +0330

    Path variables close #10

--- src/main/java/com/example/spring6reactive/services/BeerService.java
+++ src/main/java/com/example/spring6reactive/services/BeerService.java
@@ -2,7 +2,9 @@ package com.example.spring6reactive.services;
 
 import com.example.spring6reactive.model.BeerDTO;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface BeerService {
     Flux<BeerDTO> listBeers();
+    Mono<BeerDTO> getBeerById(Integer beerId);
 }
--- src/main/java/com/example/spring6reactive/services/BeerServiceImpl.java
+++ src/main/java/com/example/spring6reactive/services/BeerServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.spring6reactive.repositories.BeerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +15,11 @@ public class BeerServiceImpl implements BeerService {
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
 
+    @Override
+    public Mono<BeerDTO> getBeerById(Integer beerId) {
+        return beerRepository.findById(beerId)
+                .map(beerMapper::beerToBeerDto);
+    }
     @Override
     public Flux<BeerDTO> listBeers() {
         return beerRepository.findAll()
