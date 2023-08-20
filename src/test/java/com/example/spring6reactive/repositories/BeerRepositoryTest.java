cc0f336a01614655739c7cc7b14c087aaa9fb530
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-20 23:45:33 +0330

    Test create with postman close #12

--- src/test/java/com/example/spring6reactive/repositories/BeerRepositoryTest.java
+++ src/test/java/com/example/spring6reactive/repositories/BeerRepositoryTest.java
@@ -2,6 +2,8 @@ package com.example.spring6reactive.repositories;
 
 import com.example.spring6reactive.config.DatabaseConfig;
 import com.example.spring6reactive.domain.Beer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
@@ -16,6 +18,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class BeerRepositoryTest {
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testCreateJson() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        System.out.println(objectMapper.writeValueAsString(getTestBeer()));
+    }
 
     @Test
     void saveNewBeer(){
