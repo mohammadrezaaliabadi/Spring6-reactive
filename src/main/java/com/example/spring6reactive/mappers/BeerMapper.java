ade5f7e9ae6cbde92a0720833684f1241f168478
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-20 23:30:30 +0330

    Add mapstruct close #8

---
+++ src/main/java/com/example/spring6reactive/mappers/BeerMapper.java
@@ -0,0 +1,12 @@
+package com.example.spring6reactive.mappers;
+
+import com.example.spring6reactive.domain.Beer;
+import com.example.spring6reactive.model.BeerDTO;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+    Beer beerDtoToBeer(BeerDTO dto);
+
+    BeerDTO beerToBeerDto(Beer beer);
+}
