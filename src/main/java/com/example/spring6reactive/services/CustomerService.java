ddd4d8716f09b433c8902a64a62ca731c9d305f1
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-21 17:27:48 +0330

    Add customer Service close #27

---
+++ src/main/java/com/example/spring6reactive/services/CustomerService.java
@@ -0,0 +1,19 @@
+package com.example.spring6reactive.services;
+
+import com.example.spring6reactive.model.CustomerDTO;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface CustomerService {
+    Flux<CustomerDTO> listCustomers();
+
+    Mono<CustomerDTO> getCustomerById(Integer customerId);
+
+    Mono<CustomerDTO> saveNewCustomer(CustomerDTO customerDTO);
+
+    Mono<CustomerDTO> updateCustomer(Integer customerId, CustomerDTO customerDTO);
+
+    Mono<CustomerDTO> patchCustomer(Integer customerId, CustomerDTO customerDTO);
+
+    Mono<Void> deleteCustomerById(Integer customerId);
+}
